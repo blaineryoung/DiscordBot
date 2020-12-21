6a2c94a512f776d72849f2bea2062032df07bc08
Blaine Young <blaineryoung@gmail.com>
2020-12-21 00:45:11 -0800

    it sort of works

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+use std::fmt::Write;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = "NzkwNDkwNTU5MjQ3OTQxNjUz.X-BXsw.IulYgJtgiXGxnHsZtQHJ8n0rJHw";
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut contents = "Message info - ".to_string();
+    writeln!(contents, " name:{name}", name = msg.author.name)?;
+
+    msg.reply(ctx, contents).await?;
+
+    Ok(())
+}
